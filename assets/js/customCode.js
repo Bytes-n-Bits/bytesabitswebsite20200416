08cad8d0daf3e1263c3ef6846a72e2a6640eaaef
Bytes-n-Bits <57077047+Bytes-n-Bits@users.noreply.github.com>
2020-04-16 15:46:04 -0400

    Add files via upload

---
+++ assets/js/customCode.js
@@ -0,0 +1,13 @@
+$(document).ready(() => {
+    $('#service').on('click', () => {
+      $('#serviceMenu').show();
+  
+    })}) 
+
+
+    $(document).ready(() => {
+        $('#cart').on('click', () => {
+          $('#cartMenu').show();
+      
+        })
+      
